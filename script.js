4c2f2ff9fdd727fbed73c4b381121a11c42eef0f
Zeetbor <rob-terrins@hotmail.co.uk>
2021-01-29 20:26:48 +0000

    Added draw logic

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let board = ["", "", "", "", "", "", "", "", ""];
 let turn = "player1";
 const squares = Array.from(document.querySelectorAll('.square'));
 const message = document.querySelector('h2');
-const cover = document.querySelector('#cover');
 // const squares = document.querySelectorAll(".square")
 
 //Game Logic
@@ -32,45 +31,57 @@ function checkWin() {
   });
   if (winner != null) {
     message.innerHTML = `${winner} won the game!`;
-    cover.style.display = "flex";
+    for (const square of squares) {
+      square.removeEventListener('click', marker)
+    }
     return console.log(winner);
+  } else if (counter == 9) {
+    message.innerHTML = `It's a tie!`;
   }
 };
 
 
 //Game Board
+let counter = 0
 
-for (const square of squares) {
-  square.addEventListener('click', function(e) {
-
-    let id = squares.findIndex(function(square) {
-      return square === event.target;
-    });
-
-    if (this.innerHTML != "") {
-      return
-    } else if (turn == "player1") {
-      this.innerHTML = "X";
-      board[id] = "X";
-      message.innerHTML = "Player 2's turn:"
-      checkWin();
-      return turn = "player2"
-    } else {
-      this.innerHTML = "O";
-      board[id] = "O";
-      message.innerHTML = "Player 1's turn:"
-      checkWin();
-      return turn = "player1";
-    }
-  })
+function addListeners(){
+  for (const square of squares) {
+  square.addEventListener('click', marker)
 }
+}
+addListeners();
+
+function marker(e) {
+  let id = squares.findIndex(function(square) {
+    return square === event.target;
+  });
+
+  if (this.innerHTML != "") {
+    return
+  } else if (turn == "player1") {
+    this.innerHTML = "X";
+    board[id] = "X";
+    message.innerHTML = "Player 2's turn:"
+    counter++;
+    checkWin();
+    return turn = "player2"
+  } else {
+    this.innerHTML = "O";
+    board[id] = "O";
+    message.innerHTML = "Player 1's turn:"
+    counter++;
+    checkWin();
+    return turn = "player1";
+  }
+};
 
 const resetButton = document.querySelector(".reset")
 resetButton.addEventListener('click', function(e) {
   for (i = 0; i < squares.length; i++) {
     squares[i].innerHTML = "";
   };
+  counter = 0;
   message.innerHTML = "Player 1's turn:"
-  cover.style.display = "flex";
+  addListeners();
   return board = ["", "", "", "", "", "", "", "", ""]
 });
